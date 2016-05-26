146408fd54901f5ffa0cda33358318b9a79e9c17
Tae Hun Kim <tehunk@gmail.com>
2016-05-26 18:25:06 -0500

    hide()

--- apigo/client/apiPage/apiPage.js
+++ apigo/client/apiPage/apiPage.js
@@ -15,19 +15,19 @@ Template.apiPage.helpers({
 
 Template.apiPage.onRendered(function() {
   var LANG_LIST = $("#LANG_LIST").text().split(" ");
-  
-    $(".lang-btn-group a").click(function(){
-    $(this).attr("class", "btn btn-primary");
-    $(this).siblings().attr("class", "btn btn-default");
-    var lang_clicked = $(this).attr('id');
-    for (i=0; i<LANG_LIST.length; i++) {
-      var lang = LANG_LIST[i];
-      if (lang_clicked == lang) {
-        $("." + lang).show();
-      } else {
-        $("." + lang).hide();
-      }
-    }
-    });
+  for (var i = 1; i< LANG_LIST.length; i++) {
+	  var lang = LANG_LIST[i];
+	  $("." + lang).hide();
+  }
   
 }); 
+
+Template.apiPage.afterLoad = function(){
+	Meteor.defer(function(){
+		 var LANG_LIST = $("#LANG_LIST").text().split(" ");
+  for (var i = 1; i< LANG_LIST.length; i++) {
+	  var lang = LANG_LIST[i];
+	  $("." + lang).hide();
+  }
+	});
+}
