4e0fcf956946222ab2f5838ba2158a3f458b30d2
Shikhar Mohan <shikharmohan2016@u.northwestern.edu>
2016-06-02 14:11:29 -0500

    filledi n content on homepage

--- apigo/client/homepage/singleApi.js
+++ apigo/client/homepage/singleApi.js
@@ -1,7 +1,9 @@
-Template.singleApi.onRendered(function() {
+Template.homepage.onRendered(function() {
 	$('.owl-carousel').owlCarousel({
 		loop:true,
 		margin:10,
 		items:1,
+    autoplay:true,
+    autoplayTimeout: 2000
 	})
 });
